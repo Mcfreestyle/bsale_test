48cc40b600e3803a322ac1efe214045acc11dcbb
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-10-16 21:59:53 -0500

    feat: handle asynchrony

--- src/js/index.js
+++ src/js/index.js
@@ -1,107 +1,68 @@
-const products = getProducts();
-const searchBotton = $('form>button');
-// const API_URL = 'http://...';
+const API_URL = 'http://bsale-test-bckend.herokuapp.com/api';
 
-displayCategories();
-displayProducts(products);
+initialLoad();
 
-$('#categories #category').click(function (e) {
-  const id = e.target.dataset.id;
-  const products = getProducts(id);
+/**
+ * Start first render
+ */
+async function initialLoad() {
+  const products = await getProducts();
+  
+  await displayCategories();
   displayProducts(products);
-});
+  listenEvents();
+}
 
-searchBotton.click(searchProducts);
+/**
+ * Listen events in the loaded document
+ */
+function listenEvents() {
+  const searchBotton = $('form>button');
+  
+  $('#categories #category').click(async function (e) {
+    const id = e.target.dataset.id;
+    const products = await getProducts(id);
 
-function searchProducts(e) {
+    $('#selectCategory').html(e.target.text);
+    displayProducts(products);
+  });
+  searchBotton.click(searchProducts);
+}
+
+/**
+ * Find products that match to input value and display these products
+ * @param {*} e - Event
+ */
+async function searchProducts(e) {
   e.preventDefault();
   const input = $('#input')[0];
 
-  console.log(input.value);
-  const products = [
-    {
-      name: 'inka kola',
-      price: '$14'
-    },
-    {
-      name: 'cheetos',
-      price: '$4'
-    }
-  ];
-
+  products = await requestAPI(`/products?search=${input.value}`);
   displayProducts(products);
 }
 
-function getProducts(categoryId) {
+/**
+ * Get products according the category id passed
+ * @param {Number} categoryId - Category id
+ * @returns {Array} - All table products or all products of a category
+ */
+async function getProducts(categoryId) {
   let products = [];
 
   if (!categoryId) {
-    products = [
-      {
-        name: 'coca cola',
-        price: '$10'
-      },
-      {
-        name: 'papas fritas',
-        price: '$4'
-      },
-      {
-        name: 'vodka 1L',
-        price: '$20'
-      },
-      {
-        name: 'vino',
-        price: '$30'
-      }
-    ];
+    products = await requestAPI('/products');
   } else {
-    products = [
-      {
-        name: 'papas fritas pequeño',
-        price: '$10'
-      },
-      {
-        name: 'mani salado',
-        price: '$4'
-      },
-      {
-        name: 'cheesee',
-        price: '$20'
-      },
-      {
-        name: 'lays',
-        price: '$30'
-      }
-    ];
+    products = await requestAPI(`/categories/${categoryId}/products`);
   }
   return products;
 }
 
-function displayCategories() {
-  const categories = [
-    {
-      id: 1,
-      name: 'piscos'
-    },
-    {
-      id: 2,
-      name: 'vinos'
-    },
-    {
-      id: 3,
-      name: 'snack'
-    }
-  ];
-  // $.getJSON(API_URL + '/categories', (categories) => {
-  //   for (const category of categories) {
-  //     const item = $('<li></li>');
-  //     const link = $(`<a class="dropdown-item" data-id="${category.id}" id="category"></a>`);
-  
-  //     link.text(category.name);
-  //     item.append(link);
-  //     $('#categories').append(item);
-  //   }
-  // });
+/**
+ * Show all categories in a dropdown
+ */
+async function displayCategories() {
+  const categories = await requestAPI('/categories');
+
   for (const category of categories) {
     const item = $('<li></li>');
     const link = $(`<a class="dropdown-item" data-id="${category.id}" id="category"></a>`);
@@ -112,24 +73,19 @@ function displayCategories() {
   }
 }
 
+/**
+ * Display products in document
+ * @param {Array} products - Products data
+ */
 function displayProducts(products) {
   $('#products').empty();
   for (const product of products) {
-    const productCol = createProductColumn(product.name, product.price);
+    const productCol = createProductColumn(
+      product.name,
+      product.url_image,
+      product.price,
+      product.discount
+    );
     $('#products').append(productCol);
   }
-}
-
-function createProductColumn(name, price) {
-  const col = $('<div class="col"></div>');
-  const card = $('<div class="card h-100"></div>');
-  const img = $('<img class="card-img-top" alt="...">');
-  const cardBody = $('<div class="card-body"></div>');
-  const productName = $('<h5 class="card-title"></h5>').text(name);
-  const productPrice = $('<p class="card-text"></p>').text(price);
-
-  cardBody.append(productName, productPrice);
-  card.append(img, cardBody);
-
-  return (col.append(card));
 }
